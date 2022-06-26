2bbb62ff8e7c8c25dbfa2d1d997d8e3da878be1c
Lukasz Pietrzykowski <lukaszpietrzykowsk@gmail.com>
2022-06-26 15:56:09 +0200

    waves and linear gradient haas been updated

--- color.js
+++ color.js
@@ -5,13 +5,24 @@ purple/red: 900C3F;
 purple: 511845;
 */
 
-const arrayOfColors = ['#FF5733', '#C70039', '#511845', '#900C3F']
+const arrayOfColors = ['#FF5733', '#C70039', '#511845', '#900C3F', '#cc3f21', '#f25c3d', '#de0744', '#c7063d', '#691f59', '#4d1841']
+
+
 
 const shapeOfWAves = document.querySelectorAll('use');
+const buttonColor = document.querySelector('button');
+const navigation = document.querySelector('.navigation');
 
+window.addEventListener('DOMContentLoaded', () => {
+    const min = 0;
+    const max = 10;
+    //background: linear-gradient(0.25turn, #3f87a6, #ebf8e1, #f69d3c);
+    navigation.style.background = `linear-gradient(0.25turn, ${arrayOfColors[Math.floor(Math.random() * (max - min)) + min]}, ${arrayOfColors[Math.floor(Math.random() * (max - min)) + min]}, ${arrayOfColors[Math.floor(Math.random() * (max - min)) + min]})`;
+    document.body.style.backgroundColor = arrayOfColors[Math.floor(Math.random() * (max - min)) + min];
 
-document.body.style.backgroundColor = '#FF5733';
-shapeOfWAves[0].style.fill = '#FF5733';
-shapeOfWAves[1].style.fill = '#C70039';
-shapeOfWAves[2].style.fill = '#511845';
-shapeOfWAves[3].style.fill = '#900C3F';
+    buttonColor.style.backgroundColor = arrayOfColors[Math.floor(Math.random() * (max - min)) + min];
+    for (let i=0; i<shapeOfWAves.length; i++){
+        shapeOfWAves[i].style.fill = arrayOfColors[Math.floor(Math.random() * (max - min)) + min];
+    }
+})
+
