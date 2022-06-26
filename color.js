655a8363defc42aa3ea8fa3ade03ae8cf3638ed6
Lukasz Pietrzykowski <lukaszpietrzykowsk@gmail.com>
2022-06-26 14:25:21 +0200

    added some colors

---
+++ color.js
@@ -0,0 +1,17 @@
+/*
+orange: FF5733;
+red: C70039;
+purple/red: 900C3F;
+purple: 511845;
+*/
+
+const arrayOfColors = ['#FF5733', '#C70039', '#511845', '#900C3F']
+
+const shapeOfWAves = document.querySelectorAll('use');
+
+
+document.body.style.backgroundColor = '#FF5733';
+shapeOfWAves[0].style.fill = '#FF5733';
+shapeOfWAves[1].style.fill = '#C70039';
+shapeOfWAves[2].style.fill = '#511845';
+shapeOfWAves[3].style.fill = '#900C3F';
